fe83a5e6c2f3d0124537d0c99290dfeff5c425c5
Ian Muisyo Ngila <ngilaian@gmail.com>
2022-06-23 08:14:58 +0300

    adding eventlistener to the second button

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,7 @@ const jokeEl = document.getElementById('joke');
 const jokeBtn = document.getElementById('jokeBtn');
 
 const btn = document.getElementById('btn');
+btn.addEventListener('click', function onClick(event) {
+ 
+    event.target.style.backgroundColor = 'salmon';
+  });
