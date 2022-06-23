ecc82d5f2a4481fe99094165186773060f90e4de
Ian Muisyo Ngila <ngilaian@gmail.com>
2022-06-23 20:25:16 +0300

    added toggle thumbs up button

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,8 +4,12 @@ const jokeBtn = document.getElementById('jokeBtn');
 const btn = document.getElementById('btn');
 btn.addEventListener('click', function onClick(event) {
  
-    event.target.style.backgroundColor = 'salmon';
+    event.target.style.backgroundColor =  'dodgerblue';
   });
+
+  function myFunction(x) {
+    x.classList.toggle("fa-thumbs-down");
+  }
 
   const apiURL="https://v2.jokeapi.dev/joke/Any?blacklistFlags=religious,sexist&type=single"
 
